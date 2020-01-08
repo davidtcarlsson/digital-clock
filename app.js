f0e56c87265f491fadee06a3293d84a57da1464a
David Carlsson <carlssondavid@outlook.com>
2020-01-08 15:11:58 +0100

    Added the clock functionality

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const clock = document.querySelector(".clock");
+const showTime = () => {
+  const date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-clock.innerHTML = "hello";
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+
+  const format = `${hour}:${min}:${sec}`;
+  const clock = document.querySelector(".clock");
+
+  clock.innerHTML = format;
+
+  setTimeout(showTime, 1000);
+};
+
+showTime();
